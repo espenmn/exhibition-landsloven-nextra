8ccbe1c5f238f3a3329bf8d6b8603f50c12fd4f1
Espen Moe-Nilssen <espen@medialog.no>
2024-05-16 11:41:19 +0200

    Lagt til Card component for å bruke i layout

---
+++ components/card.tsx
@@ -0,0 +1,20 @@
+
+import * as HvalContent from 'pages/landsleiebolken/hval.no.mdx';
+
+const Card = ({ obj }: { obj?: object }) => {
+ 
+
+  return (
+    <article>
+      <div class="max-w rounded overflow-hidden shadow-lg my-4">
+      <img class="w-full" src="https://i0.wp.com/www.visitfrosta.no/wp-content/uploads/2021/02/Tinghaugen-15.jpg" alt="Trell" />
+      <div class="p-4">
+            {  obj }
+
+      </div>
+    </div>
+    </article>
+  );
+};
+
+export default Card;
