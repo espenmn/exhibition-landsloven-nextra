42e6c53963010f1d6066033e7e917d39dea02c39
Espen Moe-Nilssen <espen@medialog.no>
2024-05-22 16:05:58 +0200

    Split sub-page in componenets

--- components/sub-pages.tsx
+++ components/sub-pages.tsx
@@ -2,6 +2,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { Meta } from 'nextra';
+import SubBox from  '/components/subbox';
 
 const SubPages = ({ meta }: { meta: Meta }) => {
   const subPages = Object.entries(meta).map(([key, value], i) => {
@@ -17,34 +18,16 @@ const SubPages = ({ meta }: { meta: Meta }) => {
     // This is stupid, but since we get the links from _meta.json it gets messy. Should be a own config file.
     if (i < 2 || type) return null
 
+    // TO DO: SET OPTION  to change layout (SubBox to something else)
+
     return (
-      <div
-        key={path}
-        className='flex flex-row gap-3 border border-gray-200'
-      >
-        <div className="w-1/2">
-        <Link
-            href={path}
-          >
-            <Image 
-              src={imageurl} 
-              alt={value.title} 
-              width={0} 
-              height={0} 
-              class="w-full object-cover aspect-[1/1] "
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          />
-        </Link>
-        </div>
-        <div className="w-1/2 m-2">
-          <Link
-            href={path}
-          >
-            <h2 className='py-1 text-xl  nx-font-bold hover:underline text-red-700 hover:text-red-800'>{title}</h2> 
-          </Link>
-          <p className="text-s">{desc}</p>
-          </div>
-      </div>
+       <SubBox 
+        title={title}
+        path={path}
+        desc={desc}
+        imageurl={imageurl}
+        alttext={title}
+       />
     )
   })
 
---
+++ components/subbox.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image';
+import Link from 'next/link';
+//probably dont need meta
+// import { Meta } from 'nextra';
+
+// NULL CHECK HERE ???
+//path string or url
+
+const SubBox= ({ title, desc, imageurl, path }: { title?: string, desc?: string, imageurl?: string, path?: string  }) => {
+  return (
+      <div
+        key={path}
+        className='flex flex-row gap-3 border border-gray-200'
+      >
+        <div className="w-1/2">
+        <Link
+            href={path}
+          >
+            <Image 
+              src={imageurl} 
+              alt={title} 
+              width={0} 
+              height={0} 
+              class="w-full object-cover aspect-[1/1] "
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          />
+        </Link>
+        </div>
+        <div className="w-1/2 m-2">
+          <Link
+            href={path}
+          >
+            <h2 className='py-1 text-xl  nx-font-bold hover:underline text-red-700 hover:text-red-800'>{title}</h2> 
+          </Link>
+          <p className="text-s">{desc}</p>
+          </div>
+      </div>
+    );
+  };
+
+export default SubBox;
