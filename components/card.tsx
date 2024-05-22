080f595f38e9fe870e52b639607c481dd69d83bb
Espen Moe-Nilssen <espen@medialog.no>
2024-05-22 12:53:26 +0200

    Sync files from main fork

--- components/card.tsx
+++ components/card.tsx
@@ -1,11 +1,19 @@
+import Image from 'next/image'
+
+// image should have width / height (but we dont know that, so we need to set fill 
+
 const Card = ({ obj, imageurl, alttext }: { obj?: object, imageurl?: string, alttext?: string  }) => {
   return (
       <article>
         <div class="max-w rounded overflow-hidden shadow-lg bg-white">
         <img class="w-full" src={imageurl} alt={alttext} />
+        <Image
+          src={imageurl}
+          alt={alttext}
+          fill={true}
+        />
         <div class="p-4">
-              {  obj }
-  
+          { obj }
         </div>
       </div>
       </article>
@@ -16,4 +24,4 @@ const Card = ({ obj, imageurl, alttext }: { obj?: object, imageurl?: string, alt
     
     
    
- 
+  
