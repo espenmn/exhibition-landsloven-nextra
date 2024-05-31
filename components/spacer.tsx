619c4c134de7ca32c4bd069ad9947949b4542d5e
Espen Moe-Nilssen <espen@medialog.no>
2024-05-31 11:22:45 +0200

    Fix dynamic classes, remove 'design bolk'

--- components/spacer.tsx
+++ components/spacer.tsx
@@ -1,17 +1,11 @@
 interface SpacerProps {
-  height: string;
-  width: string;
+  height?: string | number;
+  width?: string | number;
 }
 
-const Spacer: React.FC<SpacerProps> = (props) => {
-  const { height = 24, width = 4 } = props;
-
+const Spacer: React.FC<SpacerProps> = ({ height = 24, width = 4 }) => {
   return (
-    <div className={`h-${String(height)} w-${String(height)}`} > 
- 
-    </div>
-   
-    
+    <div style={{ height: `${height}px`, width: `${width}px` }}></div>
   );
 };
 
